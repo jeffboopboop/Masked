78d5f81afbc091e4420fa9fa2dc530af14d5098e
Jeff <Jeff0012@hotmail.com>
2024-08-12 16:26:35 -0400

    update popup/js/masked.js

--- popup/js/masked.js
+++ popup/js/masked.js
@@ -10,8 +10,8 @@
             .then(response => response.json())
             .then(data => console.log(data))
             .catch(error => console.error(error));        
-        console.log(data);
-        return;
+        
+        let lines = data.split("\n");
         let secrets = [];
         let lst_count = 0;
 
@@ -26,7 +26,7 @@
             if (which.match(/regexes.txt$/)) {
                 selected_list = document.getElementById("regex-list");
                 new_id = "lst_rgx_";
-            } else {
+            } else if(which.match(/secrets$/)) {
                 selected_list = document.getElementById("seecrets-list");
                 new_id = `lst_sec_`;
             }
@@ -60,24 +60,6 @@
     
         load_lists(regexes_resource);
         load_lists(secrets_resource);
-
-
-        let secrets_list = document.getElementById("seecrets-list");
-        let regexes_list = document.getElementById("regex-list");
-
-        let secret_menu_item = document.getElementById("list-seecrets-list");
-        let regex_menu_item  = document.getElementById("list-regex-list");
-
-        let secrets_badge = document.createElement('span');
-        secrets_badge.className = "badge text-bg-warning rounded-pill float-end";
-        secrets_badge.innerText = secrets_list.length;
-
-        let regex_badge = document.createElement('span');
-        regex_badge.className = "badge text-bg-warning rounded-pill float-end";
-        regex_badge.innerText = regexes_list.length;
-        
-        secret_menu_item.appendChild(secrets_badge);
-        regex_menu_item.appendChild(regex_badge);
 
         document.querySelectorAll('option[id^="lst_sec"]').forEach(
             function(secret) {
