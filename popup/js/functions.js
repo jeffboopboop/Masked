09df22e388beef7658096d3e8eef8a003e6e0195
Jeff <Jeff0012@hotmail.com>
2024-08-12 16:26:47 -0400

    update popup/js/functions.js

--- popup/js/functions.js
+++ popup/js/functions.js
@@ -1,6 +1,24 @@
 console.log("loaded functions.js");
 function add_menu_badges() {
+    ts(secrets_resource);
 
+
+        let secrets_list = document.getElementById("seecrets-list");
+        let regexes_list = document.getElementById("regex-list");
+
+        let secret_menu_item = document.getElementById("list-seecrets-list");
+        let regex_menu_item  = document.getElementById("list-regex-list");
+
+        let secrets_badge = document.createElement('span');
+        secrets_badge.className = "badge text-bg-warning rounded-pill float-end";
+        secrets_badge.innerText = secrets_list.length;
+
+        let regex_badge = document.createElement('span');
+        regex_badge.className = "badge text-bg-warning rounded-pill float-end";
+        regex_badge.innerText = regexes_list.length;
+        
+        secret_menu_item.appendChild(secrets_badge);
+        regex_menu_item.appendChild(regex_badge);
 }
 
 function status_message(message) {
